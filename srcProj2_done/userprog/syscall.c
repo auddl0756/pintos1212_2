0d107f56356731ab82e5112f5d133ecf6f7d7be5
auddl0756 <cjddl789123@naver.com>
2019-12-13 16:36:06 +0900

    argument passing done,syscall(write,exit) impl

---
+++ srcProj2_done/userprog/syscall.c
@@ -0,0 +1,109 @@
+#include "userprog/syscall.h"
+#include <stdio.h>
+#include <syscall-nr.h>
+#include "threads/interrupt.h"
+#include "threads/thread.h"
+
+#include "threads/vaddr.h"
+#include "threads/init.h"
+#include "userprog/process.h"
+#include <list.h>
+#include "filesys/file.h"
+#include "filesys/filesys.h"
+#include "threads/palloc.h"
+#include "threads/malloc.h"
+#include "devices/input.h"
+#include "threads/synch.h"
+
+
+static void syscall_handler (struct intr_frame *);
+
+void
+syscall_init (void) 
+{
+  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
+
+static void
+syscall_handler (struct intr_frame *f )//deleted UNUSED 
+{
+ printf("syscall num : %d\n", *(uint32_t *)(f->esp));//add
+  printf ("system call!\n");
+  hex_dump(f->esp,f->esp,100,1); //added
+  switch (*(uint32_t *)(f->esp)) {
+    case SYS_HALT:
+      break;
+    case SYS_EXIT:
+      exit(*(uint32_t *)(f->esp + 20));
+      break;
+    case SYS_EXEC:
+      break;
+    case SYS_WAIT:
+      break;
+    case SYS_CREATE:
+      break;
+    case SYS_REMOVE:
+      break;
+    case SYS_OPEN:
+      break;
+    case SYS_FILESIZE:
+      break;
+    case SYS_READ:
+      break;
+    case SYS_WRITE:
+      write((int)*(uint32_t *)(f->esp+20), (void *)*(uint32_t *)(f->esp + 24), (unsigned)*((uint32_t *)(f->esp + 28)));
+      break;
+    case SYS_SEEK:
+      break;
+    case SYS_TELL:
+      break;
+    case SYS_CLOSE:
+      break;
+  //thread_exit ();
+}
+}
+
+void exit (int status) {
+printf("%s: exit(%d)\n", thread_name(), status);
+  thread_exit ();
+}
+int write (int fd, const void *buffer, unsigned size) {
+
+/*
+  if (fd == 1) {  
+   putbuf(buffer, size);
+    return size;
+  }
+*/
+  return -1; 
+}
+
+
+void halt (void) {
+  shutdown_power_off();
+}
+
+
+pid_t exec (const char *cmd_line) {
+  return process_execute(cmd_line);
+}
+
+int wait (pid_t pid) {
+  return process_wait(pid);
+}
+
+int read (int fd, void* buffer, unsigned size) {
+  int i;
+  if (fd == 0) {
+    for (i = 0; i < size; i ++) {
+      if (((char *)buffer)[i] == '\0') {
+        break;
+      }
+    }
+  }
+  return i;
+}
+
+
+
+
